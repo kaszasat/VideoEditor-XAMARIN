e4ea3073ccf083d34e2d9873d2a756b8bc225025
kaszasat <134259112+kaszasat@users.noreply.github.com>
2023-05-22 08:12:23 +0200

    Add files via upload

---
+++ VideoEditor/VideoEditor/View/NoInternetPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace VideoEditor.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public sealed partial class NoInternetPage : ContentPage
+    {
+        public NoInternetPage()
+        {
+            InitializeComponent();
+            this.BindingContext = new ViewModel.NoInternetViewModel(this);
+        }
+    }
+}
